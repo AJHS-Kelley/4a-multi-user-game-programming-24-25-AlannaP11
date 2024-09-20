6e753b24791587ea3520df3be51d45492e9f8092
AlannaP11 <alannazgamez@gmail.com>
2024-09-20 14:51:41 -0400

    Update rockPaperScissors.cs

--- 01_gaming_exercises/04_rock_paper_scissors/rockPaperScissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/rockPaperScissors.cs
@@ -1,38 +1,64 @@
 using System;
 class RockPaperScissors {
   static void Main() {
+    //player variables
     int playerScore = 0;
-    int cpuScore = 0;
     string choiceTextInput;
-    int playerChoice;
+    string playerChoice;
+    //cpu variables
     int cpuChoice;
+    int cpuScore = 0;
 
-    Console.WriteLine("Welcome to rock, paper, scissors. The rules are simple. Scissors beats paper, paper beats rock, and rock beats scissors. The winner earns a point each round. If it ends in a draw, no points are awarded. The first player to get 5 points wins.What's your name? Type it in the console and press enter.");
+    Console.WriteLine("Welcome to rock, paper, scissors. The rules are simple. Scissors beats paper, paper beats rock, and rock beats scissors. The winner earns a point each round. If it ends in a draw, no points are awarded. The first player to get 5 points wins. \nWhat's your name? Type it in the console and press enter.");
     string playerName = Console.ReadLine();
-    Console.WriteLine("Your name is " + playerName);
-    Console.WriteLine("So, " + playerName + ", What will you choose?");
-    choiceTextInput = Console.ReadLine();
-        if (choiceTextInput == "rock" || choiceTextInput == "Rock" || choiceTextInput == "ROCK")
-    {
-      playerChoice = 1;
-      Console.WriteLine($"{playerName} chose Rock!");
-    }
-    else if (choiceTextInput == "paper" || choiceTextInput == "Paper" || choiceTextInput == "PAPER")
-    {
-      playerChoice = 2;
-      Console.WriteLine($"{playerName} chose Paper!");
-    }
-    else if (choiceTextInput == "scissors" || choiceTextInput ==  "Scissors" || choiceTextInput == "SCISSORS")
-    {
-      playerChoice = 3;
-      Console.WriteLine($"{playerName} chose Scissors!");
-    }
-    else
-    {
-        Console.WriteLine("You may have made a typo. Please try again.");
-    }
-
+    Console.WriteLine($"Your name is {playerName}");
+    Console.WriteLine($"So, {playerName}, What will you choose?");
+    
     Random rnd = new Random();
+
+  while(playerScore < 5 && cpuScore < 5)
+    {
+//player selects here
+    //   choiceTextInput = Console.ReadLine().ToLower();
+    //     if (choiceTextInput == "rock")
+    // {
+    //   playerChoice = 1;
+    //   Console.WriteLine($"{playerName} chose Rock!");
+    // }
+    // else if (choiceTextInput == "paper")
+    // {
+    //   playerChoice = 2;
+    //   Console.WriteLine($"{playerName} chose Paper!");
+    // }
+    // else if (choiceTextInput == "scissors")
+    // {
+    //   playerChoice = 3;
+    //   Console.WriteLine($"{playerName} chose Scissors!");
+    // }
+    // else
+    // {
+    //     Console.WriteLine("You may have made a typo. Please try again.");
+    // }
+
+           playerChoice = Console.ReadLine().ToLower();
+         if (playerChoice == "rock")
+     {
+       Console.WriteLine($"{playerName} chose Rock!");
+     }
+     else if (playerChoice == "paper")
+     {
+       Console.WriteLine($"{playerName} chose Paper!");
+     }
+     else if (playerChoice == "scissors")
+     {
+       Console.WriteLine($"{playerName} chose Scissors!");
+     }
+     else
+     {
+         Console.WriteLine("You may have made a typo. Please try again.");
+     }
+
+    //cpu selects here
     cpuChoice = rnd.Next(1, 3);
     Console.WriteLine(cpuChoice);
             if (cpuChoice == 1)
@@ -48,45 +74,44 @@ class RockPaperScissors {
       Console.WriteLine("CPU chose Scissors!");
     }
 
-     while(playerScore <= 5 || cpuScore <=5)
-    {
+      //player's choice and cpu's choice are compared, scores are printed
       
-      if (playerChoice == 1 && cpuChoice == 2)
+      if (playerChoice == "rock" && cpuChoice == 2)
       {
         Console.WriteLine("CPU wins!");
         cpuScore++;
         Console.WriteLine($"{playerName}'s score is {playerScore}");
         Console.WriteLine($"CPU's score is {cpuScore}");
       }
-      else if (playerChoice == 1 && cpuChoice == 3)
+      else if (playerChoice == "rock" && cpuChoice == 3)
       {
         Console.WriteLine($"{playerName} wins!");
         playerScore++;
                 Console.WriteLine($"{playerName}'s score is {playerScore}");
         Console.WriteLine($"CPU's score is {cpuScore}");
       }
-      else if (playerChoice == 2 && cpuChoice == 1)
+      else if (playerChoice == "paper" && cpuChoice == 1)
       {
         Console.WriteLine($"{playerName} wins!");
         playerScore++;
                 Console.WriteLine($"{playerName}'s score is {playerScore}");
         Console.WriteLine($"CPU's score is {cpuScore}");
       }
-      else if (playerChoice == 2 && cpuChoice == 3)
+      else if (playerChoice == "paper" && cpuChoice == 3)
       {
         Console.WriteLine("CPU wins!");
         cpuScore++;
                 Console.WriteLine($"{playerName}'s score is {playerScore}");
         Console.WriteLine($"CPU's score is {cpuScore}");
       }
-      else if (playerChoice == 3 && cpuChoice == 1)
+      else if (playerChoice == "scissors" && cpuChoice == 1)
       {
         Console.WriteLine("CPU wins!");
         cpuScore++;
                 Console.WriteLine($"{playerName}'s score is {playerScore}");
         Console.WriteLine($"CPU's score is {cpuScore}");
       }
-      else if (playerChoice == 3 && cpuChoice == 2)
+      else if (playerChoice == "scissors" && cpuChoice == 2)
       {
         Console.WriteLine($"{playerName} wins!");
         playerScore++;
@@ -100,10 +125,5 @@ class RockPaperScissors {
         Console.WriteLine($"CPU's score is {cpuScore}");
       }
     }
-
-
-
-
-
   }
 }
